6557904a36fae6b07f02ab23f4e012bcdbc4d989
Brendan <Btjones711@gmail.com>
2018-12-19 23:55:11 -0500

    Part1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  styles: ['.blue-bg{background-color:blue;}']
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  { 
+  name = 'Angular';
+  private username:string = "Test"
+  private currentUser:User = { username: "Test", email: "test@test.com" }
+  
+  private disabledInput:boolean = false
+
+  private isDisabled():boolean {
+    return this.disabledInput
+  }
+
+  private isBlueBg:boolean = true
+  private bgColor:string = 'red'
+
+  private getUsername():string {
+    return this.currentUser.username
+  }
+}
+
+interface User {
+  username:string,
+  email:string
+}
